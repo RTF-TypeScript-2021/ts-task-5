7f0ae3b98d902e0162aee14e3f39a2b75e5cd795
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-22 01:50:58 +0500

    Task 1 is solved

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -5,10 +5,34 @@
  * Когда присваивается некорректный e-mail возбуждается ошибка.
  */
 
-class Example {
-    public email: string = "";
+function emailDecorator(target: object, propertyKey: string) {
+    let email: string = this[propertyKey];
+
+    const getter = function () {
+        return email;
+    };
+
+    const setter = function (newEmail: string) {
+        const emailMatch = newEmail.match("/[0-9a-z_.-]+@[0-9a-z_.-]+.[emailMatch-z]{2,4}/i");
+        if (!newEmail || emailMatch.length === 0) {
+            throw new Error("The input e-mail is incorrect");
+        } else {
+            email = newEmail;
+            console.log("e-mail valid");
+        }
+    };
+
+    Object.defineProperty(target, propertyKey, {
+        get: getter,
+        set: setter
+    });
 }
 
-let exampleInstance = new Example();
+class Example {
+    @emailDecorator
+    public email = "";
+}
+
+const exampleInstance = new Example();
 exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
 exampleInstance.email = "misha@mail.ru"; // выводит в консоль e-mail valid
