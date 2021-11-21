76dd9f5051c6e4674783d1483bea066c581ad255
vtyric <misha.torno@gmail.com>
2021-11-22 04:26:40 +0500

    решил первый таск

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -4,11 +4,26 @@
  * Когда присваивается корректный e-mail в консоль выводится сообщение email valid.
  * Когда присваивается некорректный e-mail возбуждается ошибка.
  */
+function EmailChecker(target: object, key: string) {
+    let email: string;
+    Object.defineProperty(target, key, {
+        get: () => email,
+        set(newEmail: string) {
+            if (new RegExp("^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$").test(newEmail)) {
+                email = newEmail;
+                console.log("e-mail valid");
+            } else {
+                throw new Error("invalid e-mail");
+            }
+        }
+    });
+}
 
-class Example {
-    public email: string = "";
+export class Example {
+    @EmailChecker
+    public email: string;
 }
 
 let exampleInstance = new Example();
-exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
 exampleInstance.email = "misha@mail.ru"; // выводит в консоль e-mail valid
+exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
