232a29730c396694ed50d414845e91005b89665c
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-21 22:57:38 +0500

    Task 3 is solved

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -2,10 +2,14 @@
  * Описать каким должен быть объект X, чтобы метод работал корректно
  */
 
-function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
     return obj[key];
 }
 
-const x = undefined;
+const x = {
+        "a": 0,
+    "b": 1,
+    "m": 2
+};
 
 console.log(getProperty(x, "m")); 
