8408b1f7c9b21252d02b7f9e640bf89b708f3898
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2021-11-22 15:26:12 +0300

    task-1, task-2 solved

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -5,10 +5,35 @@
  * Когда присваивается некорректный e-mail возбуждается ошибка.
  */
 
-class Example {
-    public email: string = "";
-}
+function checkEmail(){
+    return function(target : Object, propertyKey:string | symbol){
+        let value : string;
+        const getter = function(){
+            return value;
+        };
+        const setter = function(newEmail: string){
+            const regexp = /^([a-z\d-\.]{3,20})@([a-z\d-]{3,10})\.([a-z]{2,8})$/;
+            if(!regexp.test(newEmail)) {
+                throw new Error("e-mail invalid");
+            } else {
+                value = newEmail;
+                console.log("e-mail valid");
+            }
+        };
+        Object.defineProperty(target, propertyKey,{
+            get: getter,
+            set: setter
+        })
+    }
+} 
 
-let exampleInstance = new Example();
-exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
+class Example {
+
+    @checkEmail()
+    public email: string;
+
+}
+const exampleInstance = new Example();
+//exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
 exampleInstance.email = "misha@mail.ru"; // выводит в консоль e-mail valid
+console.log(exampleInstance.email);
