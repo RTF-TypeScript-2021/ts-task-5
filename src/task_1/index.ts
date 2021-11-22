39af37c98865ae231b607165ee367132b62d46ed
Ksenia Sonina <Ksenia.Sonina@urfu.me>
2021-11-22 19:24:05 +0500

    Я хочу кушать....

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /** Задача 1
  * Реализовать декоратор, используя встроенную поддержку декораторов в TypeScript,
  * который будет реагировать на присвоение в поле email значения.
@@ -5,10 +8,31 @@
  * Когда присваивается некорректный e-mail возбуждается ошибка.
  */
 
-class Example {
-    public email: string = "";
+ function accessor(target: Object, propertyKey: string | symbol): void {
+    let _val: string = this[propertyKey];
+    Object.defineProperty(target, propertyKey, {
+        get: function() {
+            return _val;
+        },
+        set: function(value: string) {
+            const reg = /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/;
+            const index = value.search(reg);
+            if (index === -1) {
+                throw Error('Твой e-mail ИНVALID');
+            } else {
+                console.log('Твой e-mail valid');
+                _val = value;
+            }
+        }
+    })
 }
 
-let exampleInstance = new Example();
+class Example {
+    @accessor
+    public email: string;
+}
+
+const exampleInstance = new Example();
 exampleInstance.email = "fkkldfjg"; // генерирует эксепшен
 exampleInstance.email = "misha@mail.ru"; // выводит в консоль e-mail valid
+console.log(exampleInstance.email);
--- src/task_2/index.ts
+++ src/task_2/index.ts
@@ -10,7 +10,7 @@
 
 /**Базовый класс для контролов */
 abstract class Control<T> {
-    public name: string = "";
+    public name = "";
 
     protected value: T;
     /**взять значение из контрола */
@@ -20,20 +20,43 @@ abstract class Control<T> {
 }
 /**Класс описывает TextBox контрол */
 class TextBox extends Control<string> {
+    public getValue(): string {
+        return this.value;
+    }
+    public setValue(val: string): void {
+        this.value = val;
+    }
 }
 /**value контрола selectBox */
 class SelectItem {
     public value: string;
     public id: number;
+
+    constructor(id: number, value: string) {
+        this.value = value;
+        this.id = id;
+    }
 }
 
 /**Класс описывает SelectBox контрол */
 class SelectBox extends Control<SelectItem> {
+    public getValue(): SelectItem {
+        return this.value;
+    }
+    public setValue(val: SelectItem): void {
+        this.value.value = val.value;
+        this.value.id = val.id;
+    }
 }
 
 class Container {
     public instance: Control<any>;
     public type: string;
+
+    constructor(instanse: Control<any>, type: string) {
+        this.instance = instanse;
+        this.type = type;
+    }
 }
 
 /**Фабрика которая отвечает за создание экземпляров контролов */
@@ -45,15 +68,21 @@ class FactoryControl {
         this._collection = [];
     }
 
-    public register<?>(type: ?) {
-}
+    public register<T extends Control<any>>(type: new () => T) {
+        this._collection.push(new Container(new type(), type.name));
+    }
 
-public getInstance<?>(type: ?): ? {
+    public getInstance<T extends Control<any>>(type: new () => T): T {
+        if (this.existType(type.name)) {
+            return this._collection.find(x => x.type === type.name).instance as T;
+        } else {
+            throw Error(`Вы ещё не зарегистрировали тип ${type.name}`);
+        }
     }
 
     private existType(type: string) {
-    return this._collection.filter(g => g.type === type).length > 0;
-}
+        return this._collection.filter(g => g.type === type).length > 0;
+    }
 }
 
 const factory = new FactoryControl();
@@ -61,5 +90,13 @@ factory.register(SelectBox);
 
 const selectBoxInstance = factory.getInstance(SelectBox);
 
-selectBoxInstance.setValue("sdfsdf") // компилятор TS не пропускает
-selectBoxInstance.setValue(new SelectItem()) // компилятор TS пропускает
+//selectBoxInstance.setValue("sdfsdf") // компилятор TS не пропускает
+selectBoxInstance.setValue(new SelectItem(1, "бубубу")) // компилятор TS пропускает
+
+factory.register(TextBox);
+
+const textBoxInstance = factory.getInstance(TextBox);
+
+
+textBoxInstance.setValue("sdfsdf") // компилятор TS пропускает
+//textBoxInstance.setValue(new SelectItem(1, "бубубу")) // компилятор TS не пропускает
