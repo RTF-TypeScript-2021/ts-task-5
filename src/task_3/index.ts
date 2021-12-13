349dbae49ded53a29323ca0555f66f238d466593
Ksenia Sonina <Ksenia.Sonina@urfu.me>
2021-12-13 18:25:15 +0500

    Надеюсь сработает

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -2,10 +2,24 @@
  * Описать каким должен быть объект X, чтобы метод работал корректно
  */
 
-function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+ function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
     return obj[key];
 }
 
-const x = undefined;
+// Это интерфейс Гири
+interface IKettlebell {
+    mass: number;
+    massMeasurement: string;
+    haveAnyoneLifted?: boolean;
+}
 
-console.log(getProperty(x, "m")); 
+// X - это какой-то объект, который содержит определенные поля
+// Допустим он реализует интерфейс Гири таким образом:
+const x: IKettlebell = {
+    mass: 100,
+    massMeasurement: "kg"
+};
+
+// А этим методом мы проверяем есть ли в объете X поле с каким-то именем
+console.log(getProperty(x, "haveAnyoneLifted")); // --> Вернет undefined потому что у объекта Х нет этого поля
+console.log(getProperty(x, "mass")); // --> Вернет 100 потому что у объекта Х есть такое поле
--- src/task_4/index.ts
+++ src/task_4/index.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/ban-types */
 /** Задача 4
  * Реализовать декоратор с шаблонным типом, который добавляется к полю класса.
  * Декоратор должен выполнять 2 функции:
@@ -6,6 +10,7 @@
  * 		2) Проверять у передаваемого объекта наличие заполненного поля.
  * 		   Если поле не заполнено, то генерируется эксепшен.
  */
+
 
 class ValueExample1 {
     public value: string;
@@ -25,6 +30,27 @@ class ValueExample2 {
     }
 }
 
+function validate(type: new() => any, propertyName: string) {
+    return (target: { [key: string] : any }, propertyKey: keyof typeof target): void => {
+        let currentVal: unknown
+        const descriptor: PropertyDescriptor = {
+            get: function() {
+                return currentVal;
+            },
+            set: function(value) {
+                if (!(value instanceof type)) {
+                    throw new Error(`Вы хотите ввести неправильный тип данных! Надо - ${type.name}, а у вас ${typeof value}`);
+                } else if (!value[propertyName]) {
+                    throw new Error(`У вас не заданао поле ${propertyName}`);
+                } else {
+                    currentVal = value;
+                }
+            }
+        }
+        Object.defineProperty(target, propertyKey, descriptor);
+    }
+}
+
 class ValidationExample {
     @validate(ValueExample1, "id")
     public propValueExample1: any;
@@ -32,3 +58,6 @@ class ValidationExample {
     @validate(ValueExample2, "booleanProp")
     public propValueExample2: any;
 }
+
+const invalid = new ValidationExample();
+invalid.propValueExample1 = new ValueExample1("fdjughj", 2212);
